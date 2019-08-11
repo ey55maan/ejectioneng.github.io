9bbc8f5c3bebf81951c26632af175d2fdde3e788
buckyinsfo <buckyinsfo@gmail.com>
2019-08-11 15:30:06 -0700

    Updates

---
+++ precache-manifest.5fbedaa2daba9b69e8b6ae57e3357a9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0802642a4969edc9e4342169c8cac52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "cdc930544772e339d0e6",
+    "url": "/static/js/main.c188a85f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
